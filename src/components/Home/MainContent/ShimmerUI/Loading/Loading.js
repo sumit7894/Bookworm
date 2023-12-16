1e82ab2548bb0b36c332fa5ad46f411a012443f2
Sumit Maurya <smithdashdash@gmail.com>
2023-12-16 15:10:37 +0530

    added spinner

--- src/components/Home/MainContent/ShimmerUI/Loading/Loading.js
+++ src/components/Home/MainContent/ShimmerUI/Loading/Loading.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { FidgetSpinner } from 'react-loader-spinner';
+import './Loading.css'
 const Loading = () => {
   return (
-    <div>Loading</div>
+  <div className='spinner__container'>
+  <FidgetSpinner
+  visible={true}
+  height="180"
+  width="180"
+  ariaLabel="dna-loading"
+  wrapperStyle={{}}
+  wrapperClass="dna-wrapper"
+  ballColors={['#ff0000', '#00ff00', '#0000ff']}
+  backgroundColor="#F4442E"
+/>
+</div>
   )
 }
 
